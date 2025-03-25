5363882b691b430502de9fa2d63ee4caf0e63657
sergiogabriel-git <202401269255@alunos.estacio.br>
2025-03-25 13:29:27 -0300

    Create xadrez.c

---
+++ xadrez.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+  //Variaveis
+  int movimento_torre = 1;
+  int movimento_bispo = 1;
+  int movimento_rainha;
+
+  //Movimento peça torre 5 casas para direita
+  printf("TORRE\n");
+  while(movimento_torre <= 5) {
+    printf("Direita\n");
+    movimento_torre++;
+  }
+
+  //Movimento peça bispo 5 casas para cima à direita
+  printf("BISPO\n");
+  do {
+    printf("Cima\n");
+    printf("Direita\n");
+    movimento_bispo++;
+    } while(movimento_bispo <= 5)
+
+  //Movimento peça rainha 8 casas para direita
+  printf("RAINHA\n");
+  for(movimento_rainha = 1; movimento_rainha <= 8; movimento_rainha++) {
+    printf("Direita\n");
+  }
+
+  return 0;
+}
