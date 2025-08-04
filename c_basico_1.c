99e044d35ac633eb9e55aa63826b8414456043df
sergiogabriel-git <202401269255@alunos.estacio.br>
2025-08-04 14:43:32 +0000

    Exercício programação, 04/08/25

--- c_basico_1.c
+++ c_basico_1.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#define PI 3.14159
+
+int square(int largura, int altura) {
+    return(largura * altura);
+}
+
+float square_float(float largura, float altura) {
+    return(largura * altura);
+}
+
+double square_double(double largura, double altura) {
+    return(largura * altura);
+}
+
+int circle(int raio) {
+    return(raio * PI);
+}
+
+float circle_float(float raio) {
+    return(raio * PI);
+}
+
+double circle_double(double raio) {
+    return(raio * PI);
+}
 
 int main() {
-    char hello[] = "Hello, world!";
-    puts(hello);
+    printf("%d\n", square(15,2));
+    printf("%.2f\n", square_float(15.5,2.5));
+    printf("%.5f\n", square_double(15.255,2.322));
+    printf("%d\n", circle(15));
+    printf("%.2f\n", circle_float(15.5));
+    printf("%.5f\n", circle_double(15.255));
 }
